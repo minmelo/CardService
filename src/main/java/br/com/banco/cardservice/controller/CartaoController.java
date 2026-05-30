526a1c9e8eb967b2b04fe45048ad0dff0f38fddc
yasmin melo <maigbm@icloud.com>
2026-05-30 01:50:44 -0300

    add AumentoRequest, add buscaPorId

--- src/main/java/br/com/banco/cardservice/controller/CartaoController.java
+++ src/main/java/br/com/banco/cardservice/controller/CartaoController.java
@@ -1,7 +1,6 @@
 package br.com.banco.cardservice.controller;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,13 +23,12 @@ public String solicitarCartao(@RequestBody CartaoRequest request) {
     return service.solicitarCartao(request);
 }
 
-@PostMapping("/aumento")
-public String solicitarAumento() {
-    return service.solicitarAumento();
+@PostMapping("/aumento/{id}")
+public String solicitarAumento(@PathVariable Long id) {
+    return service.solicitarAumento(id);
 }
-@GetMapping
-public List<Cartao> listarCartoes() {
-    return service.listarCartoes();
+@GetMapping("/{id}")
+public Cartao buscar(@PathVariable Long id) {
+    return service.buscarPorId(id);
 }
-
 }
