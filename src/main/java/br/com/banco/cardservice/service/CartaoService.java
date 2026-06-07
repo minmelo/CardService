0f369f75b12333adbaf86b1435e2f06228218cb4
yasmin melo <maigbm@icloud.com>
2026-06-07 16:53:20 -0300

    add Dockerfile

--- src/main/java/br/com/banco/cardservice/service/CartaoService.java
+++ src/main/java/br/com/banco/cardservice/service/CartaoService.java
@@ -21,49 +21,51 @@ public class CartaoService {
     }
 
     public String solicitarCartao(CartaoRequest request) {
-    Cartao cartao = new Cartao();
-    cartao.setNumero(request.numero);
-    cartao.setCvv(request.cvv);
-    cartao.setValidade(request.validade);
-    cartao.setTitular(request.titular);
-    cartao.setLimite(request.limite);
+        if (request.limite < 0) {
+            return "Limite inválido";
+        }
+        Cartao cartao = new Cartao();
+        cartao.setNumero(request.numero);
+        cartao.setCvv(request.cvv);
+        cartao.setValidade(request.validade);
+        cartao.setTitular(request.titular);
+        cartao.setLimite(request.limite);
 
-    repository.save(cartao);
-
-    System.out.println("Salvou no banco");
-
-    return "OK";
-}
-
-public List<Cartao> listarCartoes() {
-    return repository.findAll();
-    
-}
-public String solicitarAumento(Long cartaoId) {
-    Cartao cartao = repository.findById(cartaoId).orElse(null);
-    if (cartao == null) {
-        return "Aumento negado. Cartão não encontrado.";
-    } 
-    if (!cartao.isAtivo()) {
-            return "Aumento negado. Cartão inativo.";
-    } 
-    double salarioAnterior = 5000.00; // Simulação de salário anterior
-    double salarioAtual = 6000.00; // Simulação de salário atual
-    double aumento;
-    if (salarioAtual > salarioAnterior) {
-        aumento = salarioAtual - salarioAnterior;
-        cartao.setLimite(cartao.getLimite() + aumento);
         repository.save(cartao);
-        return "Aumento concedido com sucesso";
-    }   
-    return "Aumento negado. Salário não aumentou.";
 
-}
+        System.out.println("Salvou no banco");
 
-public Cartao buscarPorId(Long id) {
-    return repository.findById(id).orElse(null);
-}
+        return "OK";
+    }
 
+    public List<Cartao> listarCartoes() {
+        return repository.findAll();
 
-    
+    }
+
+    public String solicitarAumento(Long cartaoId) {
+        Cartao cartao = repository.findById(cartaoId).orElse(null);
+        if (cartao == null) {
+            return "Aumento negado. Cartão não encontrado.";
+        }
+        if (!cartao.isAtivo()) {
+            return "Aumento negado. Cartão inativo.";
+        }
+        double salarioAnterior = 5000.00; // Simulação de salário anterior
+        double salarioAtual = 6000.00; // Simulação de salário atual
+        double aumento;
+        if (salarioAtual > salarioAnterior) {
+            aumento = salarioAtual - salarioAnterior;
+            cartao.setLimite(cartao.getLimite() + aumento);
+            repository.save(cartao);
+            return "Aumento concedido com sucesso";
+        }
+        return "Aumento negado. Salário não aumentou.";
+
+    }
+
+    public Cartao buscarPorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
 }
